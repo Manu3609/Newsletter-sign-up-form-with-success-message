5725beaa1191a21d2a58d2f1c48e5d8a147fb4ac
Fanantenana Emmanuel <emmanuel3609@gmail.com>
2023-08-28 20:45:27 +0300

    all the logics OK

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,42 @@
+const card = document.querySelector('.card')
 const form = document.querySelector("#subscribingForm")
 const mailInput = document.querySelector("#mail")
 const errorText = document.querySelector("#mailLabel span")
+const userMail = document.querySelector("#userEmail")
+const successBox = document.querySelector(".successBox")
+const dismissButton = document.querySelector('.dissmiss')
+let mailValue = ""
+
+mailInput.addEventListener('change', () => {
+    mailValue = mailInput.value
+})
 form.addEventListener('submit', (e) => {
-    errorText.classList.add("invalid")
-    mailInput.classList.add("invalid")
+    e.preventDefault()
+    if (!mailInput.value.includes("@") || !mailInput.value.includes(".")) {
+        errorText.classList.add("invalid")
+        mailInput.classList.add("invalid")
+        mailInput.value = mailValue + " "
+    } else {
+        console.log("OK")
+        errorText.classList.remove("invalid")
+        mailInput.classList.remove("invalid")
+        card.classList.add('hide')
+        userMail.innerHTML = mailValue
+        setTimeout(() => { 
+            successBox.classList.add('hide')
+            card.classList.add("none")
+        }, 500)
+    }
+    console.log('submit')
+    console.log(mailValue+" ")
+})
+dismissButton.addEventListener('click', (e) => {
+    // e.preventDefault()
+    successBox.classList.remove('hide')
+    setTimeout(() => {
+        card.classList.remove("none")
+        card.classList.remove('hide')
+        mailInput.value = ""
+        mailValue = ""
+    },500)
 })
